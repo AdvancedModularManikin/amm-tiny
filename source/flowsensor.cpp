212a72c831b3644397869410e07ba877a2b9589b
Peter O'Hanley <gus@entropicengineering.com>
2017-07-12 17:11:07 -0500

    Flow interrupt is definitely called, but Watchdog is causing problems.

--- source/flowsensor.cpp
+++ source/flowsensor.cpp
@@ -29,21 +29,23 @@ init(void)
 	PORT_SetPinMux(PORTB, 4U, kPORT_MuxAsGpio);
 	gpio_pin_config_t settings = {kGPIO_DigitalInput, 0};
 	GPIO_PinInit(GPIOB, 4U, &settings);
+	EnableIRQ(PORTB_IRQn);
 	PORT_SetPinInterruptConfig(PORTB, 4U, kPORT_InterruptEitherEdge);
-	NVIC_SetPriority(PORTB_IRQn, 5);
+	NVIC_SetPriority(PORTB_IRQn, 6);
 	pulsecounts = 0;
 }
 
 void
 PORTB_IRQHandler(void)
 {
-	uint32_t iflags = PORT_GetPinsInterruptFlags(PORTB);
-	if (iflags & (1<<4U)) {
-		pulsecounts++;
-	}
+	PORT_ClearPinsInterruptFlags(PORTB, 1<<4U);
+	//uint32_t iflags = PORT_GetPinsInterruptFlags(PORTB);
+	//if (iflags & (1<<4U)) {
+	pulsecounts++;
+	//}
 	//FIXME currently clears all flags, is this the correct approach?
 	//interrupt flags must be written with 1 to clear them
-	PORT_ClearPinsInterruptFlags(PORTB, ~0U);
+
 }
 
 void
@@ -55,8 +57,12 @@ flow_sensor_task(void *params)
 
 		float ticks = pulsecounts;
 		pulsecounts = 0;
+		//TODO change to mL/min
 		//have time, ticks, ticks/L, want L/s
-		flow_rate = (ticks / TICKS_PER_LITER) * 10;
+		//flow_rate = (ticks / TICKS_PER_LITER) * 10; // L/s
+		//to get mL/min, multiply L/s * mL/L * m/s = 1000/60
+		flow_rate = (ticks / TICKS_PER_LITER) * 10 * 1000/60;
+
 		PRINTF("flow_rate: %f\r\n", flow_rate);
 		vTaskDelay(100);
 	}
