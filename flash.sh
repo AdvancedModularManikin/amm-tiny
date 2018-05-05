ef1e2f750861987873a2413b59ddae0e89bc9dab
MP <matthew.pang@gmail.com>
2018-05-05 18:42:42 -0500

    Create flash.sh
    script to flash on AMMDK linux

---
+++ flash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+DEVICE=/dev/sdg - default for AMMDK 
+FIRMWARE="$1" #1st argument -> path to .bin file (e.g ./amm-tiny.bin)
+MNTPOINT="$2" #2nd argument -> specify a mount point for the device (e.g /mnt/ammdk created) with 'sudo mkdir /mnt/ammdk 
+sudo mkdir -p $MNTPOINT #create a mount point, dont complain if it exists
+if [ ! -e $DEVICE ]; then echo "Can't find device"; exit 1; fi # check if device exists
+sudo umount  $DEVICE #unmount the device [Linux automounts it :(]
+sudo modprobe msdos #wake msdos module
+sudo mount -t msdos $DEVICE $MNTPOINT # mount device with msdos type
+sudo cp "$FIRMWARE" "$MNTPOINT" # copy the firmware
+sync # write any data buffered in memory out to disk
+sleep 1 # we have to wait (experiment :))
+sudo umount $DEVICE # i need not explain :)
+
+#Modified from : http://karibe.co.ke/2014/04/changing-the-firmware-on-freescale-freedom-boards-in-linux/
