f756f24c8f564b897cbc7bed03fc1fc603a296ad
Peter O'Hanley <gus@entropicengineering.com>
2018-05-15 16:41:02 -0500

    Increased control frequency turn off if it would stall use equation from datasheet directly

--- source/mule-1/air_tank.cpp
+++ source/mule-1/air_tank.cpp
@@ -28,6 +28,23 @@ pi_supply(struct pi_ctl *p, float reading)
 
 struct pi_ctl pid;
 
+uint16_t stall_val = 0x400;
+
+float
+scale(float vOut)
+{
+	float vsupply = 5.0;
+	float pmax = 15.0;
+	float pmin = 0.0;
+	float pressure;
+	
+	//transfer vOut = (0.8 * vsupply)/(pmax - pmin) * (pressure - pmin) + 0.1 * vsupply
+	//transfer vOut = (0.8 * 5.0)/(15.0 - 0.0) * (pressure - 0.0) + 0.1 * 5.0
+	
+	//wolframalpha
+	return 15/8 * (2*vOut - 1);
+}
+
 void
 air_reservoir_control_task(void *params)
 {
@@ -36,28 +53,33 @@ air_reservoir_control_task(void *params)
 	
 	pid.p = 0.25;
 	pid.i = 0.01;
-	pid.isum = 3; //not really based on anything
+	pid.isum = 0;
 	pid.target = 5;
 	
 	should_24v_be_on = 1;
 	should_motor_run = 1;
 	
+	//voltage divider
+	float r1 = 1200;
+	float r2 = 2200;
+	float voldiv = r2/(r1+r2);
+	
 	for (;;) {
-		uint32_t presh = carrier_sensors[0].raw_pressure;
+		uint32_t adcRead = carrier_sensors[0].raw_pressure;
+		float voltage = ((float)adcRead) * 5.0 / (4096);
 		
 		//convert to PSI. Assume linearity
 		
-		//Pressure (PSI) = VALUE * (3/10280) - (15/8)
-		//128 is to make pressure in the office ~= 13.9
-		float psi = ((float)presh) *128* (3.0/10280.0) - (15.0/8.0);
+		float psi = scale(voltage);
 		
 		float ret = pi_supply(&pid, psi);
 		
 		//TODO convert back to 0-2^12 range for DAC
 		uint32_t val = (uint32_t) (ret*0x200);
-		dacVal = val;
+		should_motor_run = stall_val > val;
+		dacVal = val > 0xfff ? 0xfff : val;
 		
-		vTaskDelay(100);
+		vTaskDelay(50);
 	}
 	vTaskSuspend(NULL);
 }
