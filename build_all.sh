ac8869bb87d4fc44683c6483a590ab8bfd994c50
Peter O'Hanley <gus@entropicengineering.com>
2019-01-11 17:11:52 -0600

    Fixed inaccuracy in README.md, updated submod

--- build_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cmake -DCMAKE_TOOLCHAIN_FILE="armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug  .
-make -j4
-cmake -DCMAKE_TOOLCHAIN_FILE="armgcc.cmake" -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release  .
-make -j4
